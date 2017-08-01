f2b7e06a81b83df720768743ba60c83ed004a8d9
Mattia Franchetto <mt.franchetto@gmail.com>
2017-08-01 18:37:25 +0200

    Add logger to workflow factory

--- scripts/WorkflowProcessorFactory.ts
+++ scripts/WorkflowProcessorFactory.ts
@@ -1,5 +1,6 @@
 import {IWorkflowProcessor, WorkflowProcessor} from "./WorkflowProcessor";
 import {inject, injectable} from "inversify";
+import {ILogger} from "prettygoat";
 
 export interface IWorkflowProcessorFactory {
     processorFor(id: string): IWorkflowProcessor;
@@ -8,12 +9,12 @@ export interface IWorkflowProcessorFactory {
 @injectable()
 export class WorkflowProcessorFactory implements IWorkflowProcessorFactory {
 
-    constructor(@inject("RedisClient") private redisClient) {
+    constructor(@inject("RedisClient") private redisClient, @inject("ILogger") private logger: ILogger) {
 
     }
 
     processorFor(id: string): IWorkflowProcessor {
-        return new WorkflowProcessor(id, this.redisClient);
+        return new WorkflowProcessor(id, this.redisClient, this.logger);
     }
 
 }
